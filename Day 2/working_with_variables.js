0fee2008217a28564fbecb776860b62a2bfd4e27
Survine <hridaybardhan18@gmail.com>
2025-01-31 09:43:20 +0530

    day 11 and this, minor edit

--- Day 2/working_with_variables.js
+++ Day 2/working_with_variables.js
@@ -1,5 +1,5 @@
-let admin;
-let name;
-name = "John";
-admin = name;
-alert(admin);
+let user = "Hriday", age = 20, isStuduent = true;
+
+console.log(user, age, isStuduent);
+
+console.table({user, age, isStuduent});
